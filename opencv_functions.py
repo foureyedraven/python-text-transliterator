401b3c6ac22781a4cf6a5f09772c2210c744e440
Theresa <theresa@ozobot.com>
2021-01-12 16:37:40 -0800

    testing images for contour id algo

--- opencv_functions.py
+++ opencv_functions.py
@@ -9,7 +9,7 @@ pytesseract.pytesseract.tesseract_cmd = '/usr/local/Cellar/tesseract/3.05.02/bin
 def save_photo_text():
     # Read image from which text needs to be extracted 
     # convert to array of ints
-    path = 'noisy_image.jpg'
+    path = 'images/coinex_kr.jpg'
     img = cv2.imread(path)
     # nparr = np.frombuffer(photo, np.uint8)
     # convert to image array
@@ -20,16 +20,18 @@ def save_photo_text():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     
     # Performing OTSU threshold 
+    # This will decide relative contrast and 
+    # generalize regions to white or black
     ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
     
-    # Specify structure shape and kernel size.  
+    # Specify structure shape and kernel size. 
     # Kernel size increases or decreases the area  
     # of the rectangle of pixels to be detected. 
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (6, 6)) 
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)) 
     
-    # Appplying dilation on the threshold image 
+    # Applying dilation on the threshold image 
+    # Using a Closing to erode after dilation
     dilation = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, rect_kernel)
-    # dilation = cv2.dilate(closing, rect_kernel, iterations = 1) 
 
     # Finding contours 
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,  
@@ -38,10 +40,10 @@ def save_photo_text():
     # Creating a copy of image 
     im2 = img.copy() 
     
-    # A text file is created and flushed 
-    file = open("recognized.txt", "w+") 
-    file.write("") 
-    file.close() 
+    # # A text file is created and flushed 
+    # file = open("recognized.txt", "w+") 
+    # file.write("")  
+    # file.close() 
     
     # Looping through the identified contours 
     # Then bounding rectangle is cropped and passed on 
@@ -53,21 +55,21 @@ def save_photo_text():
         # Drawing a rectangle on copied image 
         rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2) 
         
-        # Cropping the text block for giving input to OCR 
-        cropped = im2[y:y + h, x:x + w] 
+        # # Cropping the text block for giving input to OCR 
+        # cropped = im2[y:y + h, x:x + w] 
         
-        # Open the file in append mode 
-        file = open("recognized.txt", "a") 
+        # # Open the file in append mode 
+        # file = open("recognized.txt", "a") 
         
-        # Apply OCR on the cropped image 
-        text = pytesseract.image_to_string(cropped, lang='eng') 
+        # # Apply OCR on the cropped image 
+        # text = pytesseract.image_to_string(cropped, lang='eng') 
         
-        # Appending the text into file 
-        file.write(text) 
-        file.write("\n") 
+        # # Appending the text into file 
+        # file.write(text) 
+        # file.write("\n") 
         
-        # Close the file 
-        file.close 
+        # # Close the file 
+        # file.close 
     
     # cv2.imwrite("dilated_noisy_5.png", dilation)
     # cv2.imwrite("image_rectangles_dilation_5.png", rect)
